5f7eb2c3e183b4b6f05ce2ef02310c859961744b
CrazySatz <79259269+CrazySatz@users.noreply.github.com>
2021-02-18 17:27:29 +0530

    Add files via upload

---
+++ TravelHistory/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  TravelHistory
+//
+//  Created by user on 18/02/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
